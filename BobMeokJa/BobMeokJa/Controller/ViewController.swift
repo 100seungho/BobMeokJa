87008cbe528555aa25fe97106d9b4f64f53e5027
100seungho <pouy1015@snu.ac.kr>
2019-12-21 11:03:54 +0900

    프로젝트 기본 설정

---
+++ BobMeokJa/BobMeokJa/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BobMeokJa
+//
+//  Created by 백승호 on 2019/12/21.
+//  Copyright © 2019 Seungho Baek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
